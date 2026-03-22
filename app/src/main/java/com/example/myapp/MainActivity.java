425bd7f5006a8ea663cc826fad2530fad499f197
Hritik <hritik@gmail.com>
2026-03-22 10:08:54 +0530

    Add UI design

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.myapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        TextView tv = new TextView(this);
-        tv.setText("Hello from Termux 🔥");
-        setContentView(tv);
+        TextView text = findViewById(R.id.textView);
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(v -> {
+            text.setText("Button Clicked 😎🔥");
+        });
     }
 }
