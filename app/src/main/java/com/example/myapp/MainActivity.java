4986a16a412f041c63ed73c7c7d6cbeaeb9a2543
Hritik <hritik@gmail.com>
2026-03-23 07:52:18 +0530

    Add Notes App UI

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,21 +2,28 @@ package com.example.myapp;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView text = findViewById(R.id.textView);
-        Button btn = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
 
-        btn.setOnClickListener(v -> {
-            text.setText("Button Clicked 😎🔥");
+        button.setOnClickListener(v -> {
+            String text = editText.getText().toString();
+            textView.setText(text);
         });
     }
 }
